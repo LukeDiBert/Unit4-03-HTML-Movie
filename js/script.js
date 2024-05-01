2e87a589d9d309c7cdce143d411c988786b7a14a
LukeDiBert <159164688+LukeDiBert@users.noreply.github.com>
2024-05-01 00:26:03 +0000

    c

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function MovieRater(){
+  var age = document.getElementById("usertemp").value;
+  var result = document.getElementById("result");
+  if (age >= 17){
+    result.innerHTML = "You can see R rated movies alone";
+  }
+  else if (age >= 13){
+    result.innerHTML = "You can see PG-13 rated movies alone";
+  }
+else if (age >= 5){
+  result.innerHTML = "You can see G or PG rated movies alone";
+}
+else if (age < 0){
+  result.innerHTML = "Please insert a valid age";
+  }
+  else if (age == ""){
+  result.innerHTML = "Please enter your age";
+  }
+}
+
